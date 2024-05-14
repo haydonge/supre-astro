84512cf534c27e95ca9935e5659e4a91d6fe9965
haydonge <haydonge@gmail.com>
2024-05-14 11:10:08 +0800

    add vercel

---
+++ src/constants.ts
@@ -0,0 +1,4 @@
+export const SITE_TITLE = 'TechPerople BLog';
+export const SITE_DESCRIPTION = 'Tell More about Astro Web build';
+export const HOMEPAGE_ARTICLE_LIMIT = 6;
+export const ARTICLES_PER_PAGE  = 6;
---
+++ src/utils.ts
@@ -0,0 +1,18 @@
+//Format date to a string
+function formatDate(date: Date) : string {
+    const options : Intl.DateTimeFormatOptions = {year: 'numeric', month :'long' , day:'numeric'}
+    return new Date(date).toLocaleDateString(undefined, options);
+  }
+
+//Capitalize First Letter.
+function capitalize(str:string):string{
+  if(typeof str !== 'string'|| str.length ===0 ){
+  return str;
+  }
+  return str.charAt(0).toUpperCase() + str.slice (1);
+}
+
+
+  export {
+    formatDate, capitalize
+  };
